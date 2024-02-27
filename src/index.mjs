9b574baa57490467b1f591f7080c0d3f0fa055bc
Mr Stone <pierre.evens16@gmail.com>
2024-02-26 20:01:12 -0500

    Implement http methods enum

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 export * from './utils.mjs'
+export * from './enums/http-methods.mjs'
 export { HttpException } from './exceptions/HttpException.mjs'
 export { LogicException } from './exceptions/LogicException.mjs'
 export { RuntimeException } from './exceptions/RuntimeException.mjs'
