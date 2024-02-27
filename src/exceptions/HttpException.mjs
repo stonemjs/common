126cae5947298428cad2e44f8abad6d1f47ec44d
Mr Stone <pierre.evens16@gmail.com>
2024-02-26 19:57:04 -0500

    Implement utils

--- src/exceptions/HttpException.mjs
+++ src/exceptions/HttpException.mjs
@@ -4,35 +4,6 @@ import { RuntimeException } from "./RuntimeException.mjs"
  * Class representing an Exception.
  */
 export class HttpException extends RuntimeException {
-
-  static suspiciousOperation (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(400, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
-  static unauthorized (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(401, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
-  static notFound (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(404, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
-  static methodNotAllowed (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(405, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
-  static tooManyRequests (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(429, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
-  static throttleRequests (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(429, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
-  static ServiceUnavailable (statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
-    return new this(503, statusMessage, headers, body, message, code, metadata, previous)
-  }
-
   constructor (statusCode, statusMessage = null, headers = [], body = null, message = null, code = null, metadata = null, previous = null) {
     super(message ?? statusMessage, code ?? `HTTP-${statusCode}`, metadata, previous)
 
---
+++ src/index.mjs
@@ -0,0 +1,4 @@
+export * from './utils.mjs'
+export { HttpException } from './exceptions/HttpException.mjs'
+export { LogicException } from './exceptions/LogicException.mjs'
+export { RuntimeException } from './exceptions/RuntimeException.mjs'
